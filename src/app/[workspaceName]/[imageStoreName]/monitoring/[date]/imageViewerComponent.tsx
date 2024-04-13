73e9772a8601b72282d0fa481acb2cc5797119a6
msrks <mshrrks@gmail.com>
2024-04-13 15:56:37 -0700

    Update ImageViewerComponent to include context menu for image actions

--- src/app/[workspaceName]/[imageStoreName]/monitoring/[date]/imageViewerComponent.tsx
+++ src/app/[workspaceName]/[imageStoreName]/monitoring/[date]/imageViewerComponent.tsx
@@ -2,9 +2,16 @@
 
 import { api } from "@/trpc/react";
 import { useIntersection } from "@mantine/hooks";
-import { Loader2 } from "lucide-react";
+import { Download, ImageIcon, Loader2, Trash2 } from "lucide-react";
 import Image from "next/image";
 import { useRef } from "react";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+import { toast } from "sonner";
 
 export function ImageViewerComponent({
   imageStoreId,
@@ -15,6 +22,8 @@ export function ImageViewerComponent({
   date?: string;
   handleImageClick?: (url: string) => void;
 }) {
+  const utils = api.useUtils();
+
   const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage } =
     api.image.getInfiniteByImageStoreId.useInfiniteQuery(
       { imageStoreId, limit: 3, date },
@@ -30,6 +39,8 @@ export function ImageViewerComponent({
 
   if (hasNextPage && entry?.isIntersecting) void fetchNextPage();
 
+  const { mutateAsync: deleteImage } = api.image.deleteById.useMutation();
+
   return (
     <div className="flex w-full grow flex-col items-center">
       <div className="flex flex-wrap items-center justify-center gap-2">
@@ -38,18 +49,45 @@ export function ImageViewerComponent({
             <div
               key={image.id}
               className="relative h-[150px] w-[200px] overflow-hidden"
-              onClick={() => handleImageClick?.(image.url)}
             >
-              <Image
-                src={image.url}
-                alt=""
-                fill
-                style={{
-                  objectFit: "cover",
-                  objectPosition: "center",
-                }}
-                sizes="200px"
-              />
+              <ContextMenu>
+                <ContextMenuTrigger>
+                  <Image
+                    src={image.url}
+                    alt=""
+                    fill
+                    style={{
+                      objectFit: "cover",
+                      objectPosition: "center",
+                    }}
+                    sizes="200px"
+                  />
+                </ContextMenuTrigger>
+                <ContextMenuContent>
+                  <ContextMenuItem
+                    onClick={() => handleImageClick?.(image.url)}
+                  >
+                    <ImageIcon className="mr-2 size-4" />
+                    Set as queryImage
+                  </ContextMenuItem>
+                  <ContextMenuItem
+                    onClick={async () => {
+                      await deleteImage({ id: image.id, url: image.url });
+                      toast.success("Image deleted");
+                      await utils.image.invalidate();
+                    }}
+                  >
+                    <Trash2 className="mr-2 size-4" />
+                    Delete
+                  </ContextMenuItem>
+                  <a href={image.downloadUrl}>
+                    <ContextMenuItem>
+                      <Download className="mr-2 size-4" />
+                      Download
+                    </ContextMenuItem>
+                  </a>
+                </ContextMenuContent>
+              </ContextMenu>
             </div>
           )),
         )}
