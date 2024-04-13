446c7a65bde4fbd8e212f27582676d36bee04733
msrks <mshrrks@gmail.com>
2024-04-13 16:07:16 -0700

    Update ImageUploader component to reset form after successful upload

--- src/app/[workspaceName]/[imageStoreName]/_components/image-uploader.tsx
+++ src/app/[workspaceName]/[imageStoreName]/_components/image-uploader.tsx
@@ -1,17 +1,27 @@
+"use client";
+
 import { uploadImage } from "@/app/actions/uploadImage";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { api } from "@/trpc/react";
+import { useRef } from "react";
 import { toast } from "sonner";
 
 export function ImageUploader({ imageStoreId }: { imageStoreId: number }) {
   // const uploadImageWithImageStoreId = uploadImage.bind(null, imageStoreId);
 
+  const utils = api.useUtils();
+  const formRef = useRef<HTMLFormElement>(null);
+
   return (
     <form
+      ref={formRef}
       action={async (f) => {
         const res = await uploadImage(imageStoreId, f);
-        toast(res.message);
+        formRef.current!.reset();
+        toast.success(res.message);
+        await utils.image.invalidate();
       }}
       className="m-10 space-y-2 p-4"
     >
