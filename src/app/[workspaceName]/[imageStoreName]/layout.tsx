b2f1d04167812a7ced88e3b2e1795070a2378bf7
msrks <mshrrks@gmail.com>
2024-04-12 17:03:48 -0700

    Add layout component for [workspaceName]/[imageStoreName]

---
+++ src/app/[workspaceName]/[imageStoreName]/layout.tsx
@@ -0,0 +1,22 @@
+import { SidebarNav } from "./_components/side-bar";
+import { getSidebarNavItems } from "./_components/sidebar-nav-items";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  params: {
+    user: string;
+    project: string;
+    projectId?: string;
+  };
+}
+
+export default async function Layout({ children, params }: LayoutProps) {
+  return (
+    <div className="flex w-full grow">
+      <aside className="hidden w-1/6 min-w-fit bg-gray-100 px-2 py-2 dark:bg-gray-900 sm:block">
+        <SidebarNav items={getSidebarNavItems(params.user, params.project)} />
+      </aside>
+      <div className="flex-1 grow px-2 py-4">{children}</div>
+    </div>
+  );
+}
