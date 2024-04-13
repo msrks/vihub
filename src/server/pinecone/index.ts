921ceaf480e7ebbd3e39361d1c28f715c0d8a658
msrks <mshrrks@gmail.com>
2024-04-13 16:50:16 -0700

    Refactor Pinecone instance variable name in index.ts

--- src/server/pinecone/index.ts
+++ src/server/pinecone/index.ts
@@ -4,8 +4,8 @@ export type Metadata = {
   imagePath: string;
 };
 
-const pinecone = new Pinecone();
+const pc = new Pinecone();
 export const vdb = (namespace: string) =>
-  pinecone.index(process.env.PINECONE_INDEX!).namespace(namespace);
+  pc.index(process.env.PINECONE_INDEX!).namespace(namespace);
 export const vdbWithMetadaba = (namespace: string) =>
-  pinecone.index<Metadata>(process.env.PINECONE_INDEX!).namespace(namespace);
+  pc.index<Metadata>(process.env.PINECONE_INDEX!).namespace(namespace);
