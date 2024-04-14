43f94c15b9f566749ab53d973a03eda446b1b89b
msrks <mshrrks@gmail.com>
2024-04-14 12:54:28 -0700

    Refactor layout component to use workspaceName and imageStoreName in sidebar navigation

--- src/app/[workspaceName]/[imageStoreName]/layout.tsx
+++ src/app/[workspaceName]/[imageStoreName]/layout.tsx
@@ -4,9 +4,8 @@ import { getSidebarNavItems } from "./_components/sidebar-nav-items";
 interface LayoutProps {
   children: React.ReactNode;
   params: {
-    user: string;
-    project: string;
-    projectId?: string;
+    workspaceName: string;
+    imageStoreName: string;
   };
 }
 
@@ -14,7 +13,12 @@ export default async function Layout({ children, params }: LayoutProps) {
   return (
     <div className="flex w-full grow">
       <aside className="hidden w-1/6 min-w-fit bg-gray-100 px-2 py-2 dark:bg-gray-900 sm:block">
-        <SidebarNav items={getSidebarNavItems(params.user, params.project)} />
+        <SidebarNav
+          items={getSidebarNavItems(
+            params.workspaceName,
+            params.imageStoreName,
+          )}
+        />
       </aside>
       <div className="flex-1 grow px-2 py-4">{children}</div>
     </div>
