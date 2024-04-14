6e329ad86a249ea4bb1b480c204130b04ea7f868
msrks <mshrrks@gmail.com>
2024-04-13 17:09:55 -0700

    Refactor uploadImage function to use trpc server and include imageStoreId and file in uploadImage.ts

--- src/app/actions/uploadImage.ts
+++ src/app/actions/uploadImage.ts
@@ -1,20 +1,11 @@
 "use server";
 
-import { db } from "@/server/db";
-import { images } from "@/server/db/schema";
-import { put } from "@vercel/blob";
+import { api } from "@/trpc/server";
 import { revalidatePath } from "next/cache";
 
 export async function uploadImage(imageStoreId: number, formData: FormData) {
   const file = formData.get("image") as File;
-  const filename = `${process.env.BLOB_NAME_SPACE!}/${file.name}`;
-  const blob = await put(filename, file, { access: "public" });
-
-  await db.insert(images).values({
-    url: blob.url,
-    downloadUrl: blob.downloadUrl,
-    imageStoreId,
-  });
+  await api.image.create({ imageStoreId, file });
   revalidatePath("/[workspaceName]/[imageStoreName]/monitoring", "page");
   return { message: "Image uploaded successfully!" };
 }
