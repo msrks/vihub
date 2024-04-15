003fbabda9296bd66032ba726950b6193d789f09
msrks <mshrrks@gmail.com>
2024-04-14 18:18:11 -0700

    Update sidebar navigation icons in sidebar-nav-items.tsx

--- src/app/[workspaceName]/[imageStoreName]/_components/sidebar-nav-items.tsx
+++ src/app/[workspaceName]/[imageStoreName]/_components/sidebar-nav-items.tsx
@@ -2,27 +2,27 @@ import { Bot, MonitorCheck, Settings, Shapes, Upload } from "lucide-react";
 
 export const getSidebarNavItems = (wsName: string, imageStoreName: string) => [
   {
-    icon: <Shapes className="mr-2 size-4" />,
+    icon: <Shapes className="mr-2 size-4 text-muted-foreground" />,
     title: "Classes",
     href: `/${wsName}/${imageStoreName}/classes`,
   },
   {
-    icon: <Upload className="mr-2 size-4" />,
+    icon: <Upload className="mr-2 size-4 text-muted-foreground" />,
     title: "Upload Images",
     href: `/${wsName}/${imageStoreName}/upload-images`,
   },
   {
-    icon: <MonitorCheck className="mr-2 size-4" />,
+    icon: <MonitorCheck className="mr-2 size-4 text-muted-foreground" />,
     title: "Monitoring",
     href: `/${wsName}/${imageStoreName}/monitoring`,
   },
   {
-    icon: <Bot className="mr-2 size-4" />,
+    icon: <Bot className="mr-2 size-4 text-muted-foreground" />,
     title: "LLM Playground",
     href: `/${wsName}/${imageStoreName}/llm-playground`,
   },
   {
-    icon: <Settings className="mr-2 size-4" />,
+    icon: <Settings className="mr-2 size-4 text-muted-foreground" />,
     title: "Settings",
     href: `/${wsName}/${imageStoreName}/settings`,
   },
