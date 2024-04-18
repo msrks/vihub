86138bab0b282bcf8089e80fc60a313291a0cbe8
msrks <mshrrks@gmail.com>
2024-04-17 17:59:24 -0700

    Refactor sidebar navigation component to dynamically render items based on type

--- src/app/[workspaceName]/[imageStoreName]/_components/side-bar.tsx
+++ src/app/[workspaceName]/[imageStoreName]/_components/side-bar.tsx
@@ -4,14 +4,13 @@ import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { type getSidebarNavItems } from "./sidebar-nav-items";
 
 interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
-  items: {
-    title: string;
-    href: string;
-    icon: JSX.Element;
-  }[];
+  items: SidebarNavItem[];
 }
+
+type SidebarNavItem = ReturnType<typeof getSidebarNavItems>[number];
 
 export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
   const pathname = usePathname();
@@ -21,7 +20,35 @@ export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
       className={cn("flex flex-col space-x-0 space-y-1", className)}
       {...props}
     >
-      {items.map((item) => (
+      {["development", "operation", "genAI"].map((type) => (
+        <>
+          <h2 className="text-md px-1 text-sm font-semibold text-muted-foreground">
+            {type === "development"
+              ? "Development"
+              : type === "operation"
+                ? "Operation"
+                : "GenAI"}
+          </h2>
+          {items
+            .filter((item) => item.type === type)
+            .map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className={cn(
+                  buttonVariants({ variant: "ghost" }),
+                  pathname === item.href
+                    ? "bg-gray-200 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-800"
+                    : "hover:bg-transparent hover:underline",
+                  "justify-start",
+                )}
+              >
+                {item.icon} {item.title}
+              </Link>
+            ))}
+        </>
+      ))}
+      {/* {items.map((item) => (
         <Link
           key={item.href}
           href={item.href}
@@ -35,7 +62,7 @@ export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
         >
           {item.icon} {item.title}
         </Link>
-      ))}
+      ))} */}
     </nav>
   );
 }
--- src/app/[workspaceName]/[imageStoreName]/_components/sidebar-nav-items.tsx
+++ src/app/[workspaceName]/[imageStoreName]/_components/sidebar-nav-items.tsx
@@ -14,40 +14,48 @@ export const getSidebarNavItems = (wsName: string, imageStoreName: string) => [
     icon: <Shapes className="mr-2 size-4 text-muted-foreground" />,
     title: "Classes",
     href: `/${wsName}/${imageStoreName}/classes`,
+    type: "development",
   },
   {
     icon: <Upload className="mr-2 size-4 text-muted-foreground" />,
     title: "Upload Images",
     href: `/${wsName}/${imageStoreName}/upload-images`,
+    type: "development",
   },
   {
     icon: <Brush className="mr-2 size-4 text-muted-foreground" />,
     title: "Annotate",
     href: `/${wsName}/${imageStoreName}/annotate`,
+    type: "development",
   },
   {
     icon: <Images className="mr-2 size-4 text-muted-foreground" />,
     title: "Dataset",
     href: `/${wsName}/${imageStoreName}/dataset`,
+    type: "development",
   },
   {
     icon: <MonitorCheck className="mr-2 size-4 text-muted-foreground" />,
     title: "Monitoring",
     href: `/${wsName}/${imageStoreName}/monitoring`,
+    type: "operation",
   },
   {
     icon: <LineChart className="mr-2 size-4 text-muted-foreground" />,
     title: "Insights",
     href: `/${wsName}/${imageStoreName}/insights`,
+    type: "operation",
   },
   {
     icon: <Bot className="mr-2 size-4 text-muted-foreground" />,
     title: "LLM Playground",
     href: `/${wsName}/${imageStoreName}/llm-playground`,
+    type: "genAI",
   },
   {
     icon: <Settings className="mr-2 size-4 text-muted-foreground" />,
     title: "Settings",
     href: `/${wsName}/${imageStoreName}/settings`,
+    type: "genAI",
   },
 ];
