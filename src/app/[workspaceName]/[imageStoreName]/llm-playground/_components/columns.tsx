51aa5e837717f274b037bd4caf5d76bfea59a3d9
msrks <mshrrks@gmail.com>
2024-04-20 15:49:29 -0700

    Refactor columns.tsx to remove ReferenceImagesCell and update column definitions

--- src/app/[workspaceName]/[imageStoreName]/llm-playground/_components/columns.tsx
+++ src/app/[workspaceName]/[imageStoreName]/llm-playground/_components/columns.tsx
@@ -4,8 +4,6 @@ import type { RouterOutputs } from "@/server/api/root";
 import type { Row, ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import Image from "next/image";
-import { api } from "@/trpc/react";
 
 export type PromptingExperiment =
   RouterOutputs["promptingExperiment"]["getAll"][number];
@@ -46,39 +44,10 @@ function ClassCell({
   );
 }
 
-function ReferenceImagesCell({
-  row: {
-    original: {
-      prompting_experiment: { id },
-    },
-  },
-}: {
-  row: Row<PromptingExperiment>;
-}) {
-  const { data } = api.promptingExperimentReferenceImage.getAll.useQuery({
-    promptingExperimentId: id,
-  });
-  return (
-    <div className="flex gap-2">
-      {data?.map((image) => (
-        <Image
-          key={image.id}
-          src={image.url}
-          width={40}
-          height={40}
-          className="h-10 w-10"
-          alt={image.description ?? ""}
-        />
-      ))}
-    </div>
-  );
-}
-
 export const columns: ColumnDef<PromptingExperiment>[] = [
   { header: "Title", cell: TitleCell },
   { header: "Class", cell: ClassCell },
   { header: "Prompt", accessorKey: "prompting_experiment.specDefinition" },
-  { header: "Reference Images", cell: ReferenceImagesCell },
   // TODO
   { header: "Score (Positive)", cell: () => <>4/5</> },
   { header: "Score (Negative)", cell: () => <>1/5</> },
