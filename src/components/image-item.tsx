a2ae9f2dc740754bf699b1c50d8357bfb371ef69
msrks <mshrrks@gmail.com>
2024-04-20 21:59:23 -0700

    Update Bot class size and position in ImageItem component

--- src/components/image-item.tsx
+++ src/components/image-item.tsx
@@ -100,7 +100,7 @@ export function ImageItem({
           )}
           {image.selectedForExperiment && (
             <Badge className="absolute bottom-0 left-0">
-              <Bot className="size-4" />
+              <Bot className="mr-1 size-3" /> LLM
             </Badge>
           )}
         </ContextMenuTrigger>
