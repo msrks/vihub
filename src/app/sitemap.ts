ea0dc672f4cb0ac59ac0d6f3f52c7098d13eaaac
msrks <mshrrks@gmail.com>
2024-04-22 21:22:43 -0700

    Update sitemap function in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,3 +2,10 @@ export const baseUrl = "https://vihub.msrks.dev";
 export const title = "VIHub";
 export const description =
   "All in one platform to collect data, create dataset, train model, deploy to edge. VIHub offers the data-centric way to develop 'Visual Inspection AI'.";
+
+export default async function sitemap() {
+  return [""].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
+}
