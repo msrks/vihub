fabc6495c6fe72342e05ba6c467c361d1c8ef737
msrks <mshrrks@gmail.com>
2024-04-23 07:53:07 -0700

    Update font size in og/route.tsx and opengraph-image.tsx

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -8,9 +8,9 @@ export async function GET(req: NextRequest) {
   return new ImageResponse(
     (
       <div tw="bg-gray-900 w-full h-full flex flex-col items-center justify-center">
-        <div tw="text-orange-600 text-5xl font-bold">{title}</div>
+        <div tw="text-orange-600 text-7xl font-bold">{title}</div>
         <img src={"https://vihub.msrks.dev/icon.png"} alt="" width="300px" />
-        <div tw="text-gray-500 text-3xl font-semibold">
+        <div tw="text-gray-500 text-5xl font-semibold">
           Better data, better AI
         </div>
       </div>
