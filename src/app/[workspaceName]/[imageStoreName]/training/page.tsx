34adbb27cf43440e8d99cbfafd31ea107364f264
msrks <mshrrks@gmail.com>
2024-05-06 08:36:28 -0700

    feat: Add training page component
    This commit adds a new component, "Training", to the application. The component is responsible for rendering the training page, which displays information about the training process for a specific workspace and image store. It also fetches data from the server using the TRPC API.

---
+++ src/app/[workspaceName]/[imageStoreName]/training/page.tsx
@@ -0,0 +1,29 @@
+import { Loader } from "@/components/ui/loader";
+import { api } from "@/trpc/server";
+import { Suspense } from "react";
+
+interface Props {
+  params: { workspaceName: string; imageStoreName: string };
+}
+
+export default async function Page({ params }: Props) {
+  return (
+    <div className="flex w-full grow flex-col items-center">
+      <div className="container flex items-center justify-between gap-2">
+        <h2 className="text-2xl font-semibold tracking-tight">Training</h2>
+      </div>
+      <Suspense fallback={<Loader />}>
+        <Training params={params} />
+      </Suspense>
+    </div>
+  );
+}
+
+async function Training({ params: { workspaceName, imageStoreName } }: Props) {
+  const imageStore = await api.imageStore.getByName({
+    workspaceName,
+    imageStoreName,
+  });
+
+  return <div>Training</div>;
+}
