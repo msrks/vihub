7e7225139074dc23b1a32d62236cd5a69f9db138
msrks <mshrrks@gmail.com>
2024-05-12 10:09:22 -0700

    refactor: Add trainingJobRouter and update model status
    This commit adds the trainingJobRouter to the appRouter in the root.ts file, providing endpoints for managing training jobs within the API. It also updates the model status by retrieving information from the Vertex AI model service and updating the corresponding training jobs in the database. These changes enhance the functionality of the application and improve its integration with Vertex AI.

--- src/server/api/routers/trainingJob.ts
+++ src/server/api/routers/trainingJob.ts
@@ -1,4 +1,4 @@
-import { and, eq, isNotNull } from "drizzle-orm";
+import { and, eq, isNotNull, notInArray } from "drizzle-orm";
 import * as fs from "fs";
 import { z } from "zod";
 
@@ -6,6 +6,11 @@ import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { images, labelClasses, trainingJobs } from "@/server/db/schema";
 import { getGCPCredentials } from "@/server/gcs";
 import { createDatasetImage, importDataImage } from "@/server/vertexai/dataset";
+import { exportModel, listModelEvaluation } from "@/server/vertexai/model";
+import {
+  listTrainingPipelines,
+  trainAutoMLImageClassification,
+} from "@/server/vertexai/pipeline";
 import { Storage } from "@google-cloud/storage";
 
 export const trainingJobRouter = createTRPCRouter({
@@ -21,6 +26,88 @@ export const trainingJobRouter = createTRPCRouter({
         .from(trainingJobs)
         .where(eq(trainingJobs.imageStoreId, imagesStoreId));
     }),
+
+  trainAllOfReady: protectedProcedure.mutation(async ({ ctx }) => {
+    // https://cloud.google.com/vertex-ai/docs/reference/rest/v1/PipelineState
+    const jobs = await ctx.db
+      .select()
+      .from(trainingJobs)
+      .where(
+        and(
+          isNotNull(trainingJobs.datasetId),
+          notInArray(trainingJobs.state, [
+            "started",
+            "PIPELINE_STATE_QUEUED",
+            "PIPELINE_STATE_PENDING",
+            "PIPELINE_STATE_RUNNING",
+          ]),
+        ),
+      );
+    await Promise.all(
+      jobs.map(async (job) => {
+        await trainAutoMLImageClassification({ datasetId: job.datasetId }),
+          await ctx.db
+            .update(trainingJobs)
+            .set({ state: "started" })
+            .where(eq(trainingJobs.id, job.id));
+      }),
+    );
+    return { numTriggerd: jobs.length };
+  }),
+
+  updateModelStatus: protectedProcedure.mutation(async ({ ctx }) => {
+    const pipelines = await listTrainingPipelines();
+    const uniqueDatasetIds = [...new Set(pipelines.map((p) => p.datasetId!))];
+
+    await Promise.all(
+      uniqueDatasetIds.map(async (dId) => {
+        const p = pipelines.find((p) => p.datasetId === dId);
+        if (!p?.state) return;
+
+        const jobs = await ctx.db
+          .select()
+          .from(trainingJobs)
+          .where(eq(trainingJobs.datasetId, dId));
+        if (!jobs[0]) return;
+
+        // already exist in pipeline
+        if (jobs[0].datasetId === p.modelId && jobs[0].state === p.state) {
+          return;
+        }
+
+        await ctx.db
+          .update(trainingJobs)
+          .set({ modelId: p.modelId, state: p.state as string })
+          .where(eq(trainingJobs.datasetId, dId));
+
+        if (p.modelId && p.state === "PIPELINE_STATE_SUCCEEDED") {
+          const modelEval = await listModelEvaluation({ modelId: p.modelId });
+          const urlTFlite = await exportModel({
+            format: "tflite",
+            modelId: p.modelId,
+          });
+          const urlSavedModel = await exportModel({
+            format: "tf-saved-model",
+            modelId: p.modelId,
+          });
+          const urlTFJS = await exportModel({
+            format: "tf-js",
+            modelId: p.modelId,
+          });
+          await ctx.db
+            .update(trainingJobs)
+            .set({
+              auPrc: modelEval?.auPrc,
+              evalId: modelEval?.id,
+              urlTFlite,
+              urlSavedModel,
+              urlTFJS,
+            })
+            .where(eq(trainingJobs.datasetId, dId));
+        }
+      }),
+    );
+  }),
 
   prepareDatasetClsS: protectedProcedure
     .input(
@@ -53,8 +140,6 @@ export const trainingJobRouter = createTRPCRouter({
 
       const gcsSourceUri = `gs://vihub/${destination}`;
 
-      console.log(gcsSourceUri);
-
       const datasetId = await createDatasetImage({
         displayName: timestamp.toString(),
       });
@@ -62,10 +147,10 @@ export const trainingJobRouter = createTRPCRouter({
 
       return await ctx.db.insert(trainingJobs).values({
         type: "clsS",
-        status: "preparing",
+        state: "preparing",
         numImages: dataset.length,
-        vertexAiDatasetId: datasetId,
-        gcsDatasetFilePath: gcsSourceUri,
+        datasetId,
+        importFilePath: gcsSourceUri,
         createdAt: new Date(),
         updatedAt: new Date(),
         imageStoreId,
