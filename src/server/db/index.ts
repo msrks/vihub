d731267581c0b608db6cfae0137f228d9b301905
msrks <mshrrks@gmail.com>
2024-05-19 18:58:36 -0700

    refactor: Update drizzle-orm configuration in db/index.ts
    The drizzle-orm configuration in the db/index.ts file is updated to include the schema and logger options. This change enhances the functionality and logging capabilities of the database connection.

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import { env } from "@/env";
+
 import * as schema from "./schema";
 
 /**
@@ -15,4 +16,7 @@ const globalForDb = globalThis as unknown as {
 const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
-export const db = drizzle(conn, { schema });
+export const db = drizzle(conn, {
+  schema,
+  // logger: true,
+});
