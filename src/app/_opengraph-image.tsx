de2d483bc4ffe0bfba06f2c192194ef5910a78c9
msrks <mshrrks@gmail.com>
2024-06-05 17:00:34 -0700

    refactor: Rename opengraph-image.tsx to _opengraph-image.tsx and update function name to OGImage

--- src/app/_opengraph-image.tsx
+++ src/app/_opengraph-image.tsx
@@ -1,4 +1,5 @@
 import { ImageResponse } from "next/og";
+
 import { title } from "./sitemap";
 
 export const runtime = "edge";
@@ -10,12 +11,13 @@ export const size = {
 };
 export const contentType = "image/png";
 
-export default async function Image() {
+export default async function OGImage() {
   return new ImageResponse(
     (
       <div tw="bg-gray-900 w-full h-full flex flex-col items-center justify-center">
         <div tw="text-orange-600 text-7xl font-bold">{title}</div>
         <img src={"https://vihub.msrks.dev/icon.png"} alt="" width="300px" />
+        {/* <Image src={icon} alt="" width={300} /> */}
         <div tw="text-gray-500 text-5xl font-semibold">
           Better data, better AI
         </div>
